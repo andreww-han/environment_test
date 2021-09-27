e527652cafb724e4bdac35faa1c67f65dd3e42a5
andreww-han <68350961+andreww-han@users.noreply.github.com>
2021-09-27 16:45:42 -0500

    Book Collection assignment complete

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -15,3 +15,48 @@ RSpec.describe Book, type: :model do
     expect(subject).not_to be_valid
   end
 end
+
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(title: 'harry potter', author: 'J.K Rowling')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.title = nil
+    expect(subject).not_to be_valid
+  end
+end
+
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(title: 'harry potter', price: '19.99')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.title = nil
+    expect(subject).not_to be_valid
+  end
+end
+
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(title: 'harry potter', published_date: '11/09/1999')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.title = nil
+    expect(subject).not_to be_valid
+  end
+end
